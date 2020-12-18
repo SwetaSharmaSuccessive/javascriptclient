99a043081f91deb6ba297fdae05b6c0c67c536dc
SwetaSharmaSuccessive <sweta.sharma@successive.tech>
2020-12-18 12:35:37 +0530

    update #40791

--- src/pages/ChildrenDemo/ChildrenDemo.jsx
+++ src/pages/ChildrenDemo/ChildrenDemo.jsx
@@ -1,116 +1,28 @@
 import React from 'react';
-import { ThemeProvider } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import { Math } from '../../components/Math';
-import Theme from '../../theme';
 
 const ChildrenDemo = () => (
   <>
-    <ThemeProvider theme={Theme} />
-    <Math
-      first={77}
-      second={33}
-      operator="*"
-    >
+    <Math first={156} second={234} operator="+" />
+    <Math first={14} second={2} operator="-" />
+    <Math first={-11} second={2} operator="*" />
+    <Math first={16.9} second={2} operator="/" />
+    <Math first={28} second={3} operator="^" />
+    <Math first={91} second={63} operator="*">
       {
         (item) => (
-          <p>
-            {' '}
-            Multiplication of
-            {' '}
-            {item.first}
-            {' '}
-            and
-            {' '}
-            {item.second}
-            {' '}
-            is
-            {' '}
-            {item.result}
-          </p>
+          <p>{`Multiplication of ${item.first} and ${item.second} is ${item.result}`}</p>
         )
       }
     </Math>
-    <Math first={465} second={632} operator="+">
+    <Math first={4} second={0} operator="/">
       {
         (item) => (
-          <p>
-            When we add
-            {' '}
-            {item.first}
-            {' '}
-            and
-            {' '}
-            {item.second}
-            {' '}
-            we get
-            {' '}
-            {item.result}
-          </p>
+          <Typography variant="h6">{`When we divide ${item.first} & ${item.second} we get ${item.result}`}</Typography>
         )
       }
     </Math>
-    <Math first={465} second={632} operator="-">
-      {
-        (item) => (
-          <p>
-            When we add
-            {' '}
-            {item.first}
-            {' '}
-            and
-            {' '}
-            {item.second}
-            {' '}
-            we get
-            {' '}
-            {item.result}
-          </p>
-        )
-      }
-    </Math>
-    <Math first={5} second={0} operator="/">
-      {
-        (item) => (
-          <p>
-            When we divide
-            {' '}
-            {item.first}
-            {' '}
-            and
-            {' '}
-            {item.second}
-            {' '}
-            we get
-            {' '}
-            {item.result}
-          </p>
-        )
-      }
-    </Math>
-    <Math first={765} second={76} operator="?">
-      {
-        (item) => (
-          <p>
-            When we perform (?)
-            {' '}
-            {item.first}
-            {' '}
-            &
-            {' '}
-            {item.second}
-            {' '}
-            we get
-            {' '}
-            {item.result}
-          </p>
-        )
-      }
-    </Math>
-    <Math
-      first={298}
-      second={384}
-      operator="+"
-    />
   </>
 );
 export default ChildrenDemo;
