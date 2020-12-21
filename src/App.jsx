de0c32f0d56afc9c50e7a94d4a64b0bd113848c1
SwetaSharmaSuccessive <sweta.sharma@successive.tech>
2020-12-21 09:59:40 +0530

    feat:Material UI - Styling, Dialog, and Form Validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { ThemeProvider } from '@material-ui/core';
-import { ChildrenDemo } from './pages';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { Trainee } from './pages/Trainee';
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <ChildrenDemo />
+      {/* <ChildrenDemo /> */}
+      <Trainee />
     </ThemeProvider>
   );
 }
+
 export default App;
